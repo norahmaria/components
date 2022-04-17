a2801562fd5e100c837ed557a3c760dec565937d
Norah Maria Hareide Nilsen <norahmariano@gmail.com>
2022-04-17 18:00:06 +0200

    Added Text Input functionality tests

--- src/components/TextInput/TextInput.test.tsx
+++ src/components/TextInput/TextInput.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, act } from '@testing-library/react'
 
 import { TextInput } from '../../components/index'
 
@@ -35,5 +35,103 @@ describe('TextInput Component', () => {
     expect(wrapper).toHaveClass('size-small')
     expect(wrapper).toHaveClass('color-success')
     expect(wrapper).toHaveClass('round-true')
+  })
+
+  it('Can run onTextInput change when input changes', () => {
+    const onTextInputChange = jest.fn()
+
+    const { getByTestId } = render(
+      <TextInput
+        label="Label"
+        id="id"
+        placeholder="Placeholder.."
+        onTextInputChange={onTextInputChange}
+        color="success"
+        size="small"
+        round={true}
+      />
+    )
+
+    const input = getByTestId('text-input')
+    const value = ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd']
+
+    const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+      window.HTMLInputElement.prototype,
+      'value'
+    ).set
+
+    for (let i = 0; i <= value.length; i++) {
+      nativeInputValueSetter.call(input, value[i])
+
+      act(() => {
+        input.dispatchEvent(new Event('input', { bubbles: true }))
+      })
+    }
+
+    expect(onTextInputChange).toHaveBeenCalledTimes(11)
+  })
+
+  it('Displays proper value', () => {
+    const onTextInputChange = jest.fn()
+
+    const { getByTestId } = render(
+      <TextInput
+        label="Label"
+        id="id"
+        placeholder="Placeholder.."
+        onTextInputChange={onTextInputChange}
+        color="success"
+        size="small"
+        round={true}
+      />
+    )
+
+    const input = getByTestId('text-input')
+    var nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+      window.HTMLInputElement.prototype,
+      'value'
+    ).set
+
+    nativeInputValueSetter.call(input, 'Hello World')
+
+    act(() => {
+      input.dispatchEvent(new Event('input', { bubbles: true }))
+    })
+
+    expect(input).toHaveValue('Hello World')
+  })
+
+  it('Can limit length with characterLimit', () => {
+    const onTextInputChange = jest.fn()
+    const { getByTestId } = render(
+      <TextInput
+        label="Label"
+        id="id"
+        placeholder="Placeholder.."
+        onTextInputChange={onTextInputChange}
+        characterLimit={3}
+      />
+    )
+
+    const input = getByTestId('text-input')
+
+    const string = ['H', 'e', 'l', 'l', 'o']
+    const value: string[] = []
+
+    const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+      window.HTMLInputElement.prototype,
+      'value'
+    ).set
+
+    for (let i = 0; i < string.length; i++) {
+      value.push(string[i])
+      nativeInputValueSetter.call(input, value.join(''))
+
+      act(() => {
+        input.dispatchEvent(new Event('input', { bubbles: true }))
+      })
+    }
+
+    expect(onTextInputChange).toHaveBeenLastCalledWith('Hel')
   })
 })
