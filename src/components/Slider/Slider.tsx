46c04859754a543de65c040570ecf0ff6113bdca
Norah Maria Hareide Nilsen <norahmariano@gmail.com>
2022-04-20 20:05:17 +0200

    Made base placebo slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import SliderProps from './Slider.types'
 
-const Slider = () => {
-  return <div className="slider"></div>
+import './Slider.scss'
+
+const Slider = ({
+  defaultValue,
+  max = 100,
+  min = 0,
+  id,
+  color = 'primary',
+}: SliderProps) => {
+  const [percentage, setPercentage] = useState(0)
+  const [dotLeftPosition, setDotLeftPosition] = useState(0)
+  const [value, setValue] = useState(defaultValue || 20)
+  const ref = useRef<HTMLDivElement>(null)
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(parseFloat(e.target.value))
+  }
+
+  useEffect(() => {
+    setPercentage(getPercentage(value, max, min))
+
+    setDotLeftPosition(() => {
+      const range = ref.current?.clientWidth
+      return (value / range) * 100
+    })
+  }, [value])
+
+  return (
+    <div className="slider" ref={ref}>
+      <label htmlFor={id} className="form-label-nm">
+        Slider
+      </label>
+      <input id={id} min={min} max={max} onChange={onChange} type="range" />
+      <div
+        className="range"
+        style={{
+          backgroundImage: `linear-gradient(90deg, var(--${color}700) ${percentage}%, #E7E9EB ${percentage}%)`,
+        }}>
+        <span
+          className="dot"
+          style={{
+            marginLeft: `${percentage > 92 ? 92 : percentage}%`,
+          }}></span>
+      </div>
+    </div>
+  )
+}
+
+const getPercentage = (number: number, max: number, min: number) => {
+  const range = max - min
+  const positiveValue = number - min
+
+  return (positiveValue / range) * 100
+  console.table([{ range, number, positiveValue }])
 }
 
 export default Slider
--- src/components/Slider/Slider.types.ts
+++ src/components/Slider/Slider.types.ts
@@ -1,3 +1,15 @@
-interface SliderProps {}
+interface SliderProps {
+  /** Unique id for the slider */
+  id: string
+  /** Add a default value */
+  defaultValue: number
+  /** Min value */
+  min?: number
+  /** Max value */
+  max?: number
+
+  /** The color scheme of the Radio buttons */
+  color?: 'primary' | 'neutral' | 'success' | 'warning' | 'error'
+}
 
 export default SliderProps
