d30b3020a6d13bf4155bb06a7c179387f2e908bc
Norah Maria Hareide Nilsen <norahmariano@gmail.com>
2022-04-24 23:53:00 +0200

    Updated button tests with naming convention

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -6,7 +6,7 @@ import { Button } from '../../components/index'
 describe('Button Component', () => {
   it('Can render', () => {
     const { getByRole } = render(
-      <Button onClick={() => {}}>Hello World</Button>
+      <Button onButtonClick={() => {}}>Hello World</Button>
     )
 
     expect(getByRole('button')).toMatchSnapshot()
@@ -15,10 +15,10 @@ describe('Button Component', () => {
   it('Can render non default variants', () => {
     const buttons = render(
       <>
-        <Button onClick={() => {}} variant="outlined">
+        <Button onButtonClick={() => {}} variant="outlined">
           Hello World
         </Button>
-        <Button onClick={() => {}} variant="secondary">
+        <Button onButtonClick={() => {}} variant="secondary">
           Hello World
         </Button>
       </>
@@ -29,7 +29,7 @@ describe('Button Component', () => {
 
   it('Can toggle click class', async () => {
     const { getByRole } = render(
-      <Button onClick={() => {}}>Hello World</Button>
+      <Button onButtonClick={() => {}}>Hello World</Button>
     )
     const button = getByRole('button')
 
@@ -43,7 +43,7 @@ describe('Button Component', () => {
 
   it('Can render loading icon', () => {
     const { getByTestId } = render(
-      <Button onClick={() => {}} isLoading={true}>
+      <Button onButtonClick={() => {}} isLoading={true}>
         Hello World
       </Button>
     )
@@ -58,10 +58,10 @@ describe('Button Component', () => {
 
     const { getByTestId, getByText } = render(
       <>
-        <Button onClick={jest.fn()} isLoading={true}>
+        <Button onButtonClick={jest.fn()} isLoading={true}>
           Loading Button
         </Button>
-        <Button onClick={jest.fn()}>Disabled Button</Button>
+        <Button onButtonClick={jest.fn()}>Disabled Button</Button>
       </>
     )
 
