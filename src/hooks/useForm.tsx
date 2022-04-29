ee8959ead9319fbbb2150c6f81a9c440783ae0c9
Norah Maria Hareide Nilsen <norahmariano@gmail.com>
2022-04-29 22:59:15 +0200

    Started useForm hook

---
+++ src/hooks/useForm.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+
+const useForm = (initial: any) => {
+  const [form, setForm] = useState(initial)
+
+  const onChange = (
+    value: string | number | (string | number)[] | boolean,
+    e?: any
+  ) => {
+    switch (e.target.type) {
+      case 'radio':
+        return setForm(form => ({
+          ...form,
+          [e.target.name]: e.target.value,
+        }))
+        break
+
+      case 'range':
+        return setForm(form => ({
+          ...form,
+          [e.target.id]: parseFloat(e.target.value),
+        }))
+        break
+
+      case 'checkbox':
+        return setForm(form => ({
+          ...form,
+          [e.target.id]: e.target.checked,
+        }))
+
+      default:
+        return setForm(form => ({
+          ...form,
+          [e.target.id]: e.target.value,
+        }))
+    }
+  }
+
+  useEffect(() => {
+    console.log(form)
+  }, [form])
+
+  return {
+    form,
+    setForm,
+    onChange,
+  }
+}
+
+export default useForm
