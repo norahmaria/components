86b73d30a81c15d1e01628498d7f31994be84346
Norah Maria Hareide Nilsen <norahmariano@gmail.com>
2022-05-02 17:30:32 +0200

    Changed useForm extension

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { onSelectionChangeArgs } from '../components/Select/Select.types'
 
 import { CustomColor } from '../components/Tag/Tag.types'
